9735df176d60a954c9a84871ddb5846e207cfa94
김민규 <53891733+stariver1119@users.noreply.github.com>
2025-06-11 22:28:22 +0900

    build_3 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Askstar/',
   server: {
     host: true, // Expose to all network interfaces
     port: 3000, // Use a specific port
